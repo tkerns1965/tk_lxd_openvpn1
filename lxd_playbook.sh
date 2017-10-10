348be2922d257497deb8c0811e62c10e5353db9a
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-10 09:44:37 -0400

    Update lxd_playbook.sh

--- lxd_playbook.sh
+++ lxd_playbook.sh
@@ -10,15 +10,23 @@ lxc exec cnt-ubuntu-base -- \
         && apt upgrade -y \
         && apt autoremove -y"
 
-# lxc restart cnt-ubuntu-base
-# lxc stop cnt-ubuntu-base
-# lxc copy cnt-ubuntu-base cnt-easyrsa-base
-# lxc start cnt-easyrsa-base
+lxc restart cnt-ubuntu-base
+lxc stop cnt-ubuntu-base
+lxc copy cnt-ubuntu-base cnt-easyrsa-base
+lxc start cnt-easyrsa-base
+sleep 10
 
 # lxc exec cnt-easyrsa-base -- wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz
 # lxc exec cnt-easyrsa-base -- tar zxvfP EasyRSA-3.0.3.tgz --transform 's!^EasyRSA-3.0.3!/etc/easyrsa!'
 # lxc exec cnt-easyrsa-base -- chown -R 0:0 /etc/easyrsa/
 # lxc exec cnt-easyrsa-base -- bash -c "cd /etc/easyrsa/ && ./easyrsa init-pki"
+
+lxc exec cnt-easyrsa-base -- \
+    bash -c "wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz \
+        && tar zxvfP EasyRSA-3.0.3.tgz --transform 's!^EasyRSA-3.0.3!/etc/easyrsa!' \
+        && chown -R 0:0 /etc/easyrsa/ \
+        && cd /etc/easyrsa/ \
+        && ./easyrsa init-pki"
 
 # lxc restart cnt-easyrsa-base
 # lxc stop cnt-easyrsa-base
