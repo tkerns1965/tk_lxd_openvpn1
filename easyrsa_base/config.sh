5b15f0461a9e36452bd8f00418cc7258f7abaabd
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-10 06:58:56 -0400

    Update config.sh

--- easyrsa_base/config.sh
+++ easyrsa_base/config.sh
@@ -3,3 +3,8 @@
 apt update
 apt upgrade -y
 apt autoremove -y
+
+wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz
+tar zxvfP EasyRSA-3.0.3.tgz --transform 's!^EasyRSA-3.0.3!/etc/easyrsa!'
+chown -R 0:0 /etc/easyrsa/
+bash -c "cd /etc/easyrsa/ && ./easyrsa init-pki"
