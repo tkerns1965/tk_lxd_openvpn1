3747da62e075fa7e1cab3c3302e7056d3aa8faff
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-11 16:58:33 -0400

    Update lxd_playbook.sh

--- lxd_playbook.sh
+++ lxd_playbook.sh
@@ -1,120 +1,137 @@
-lxc launch ubuntu:16.04 cnt-ubuntu-base
+lxc launch ubuntu:16.04 ubuntu-base
 sleep 15
 
-lxc exec cnt-ubuntu-base -- \
-    bash -c "echo 'cnt-ubuntu-base>' \
+lxc exec ubuntu-base -- \
+    bash -c "echo 'ubuntu-base>' \
         && apt update \
         && apt upgrade -y \
         && apt autoremove -y \
-        && echo '<cnt-ubuntu-base'"
+        && echo '<ubuntu-base'"
 
-lxc restart cnt-ubuntu-base
+lxc restart ubuntu-base
 
-lxc stop cnt-ubuntu-base
-lxc copy cnt-ubuntu-base cnt-easyrsa-base
+lxc stop ubuntu-base
+lxc copy ubuntu-base easyrsa-base
 
-lxc start cnt-easyrsa-base
+lxc start easyrsa-base
 sleep 10
 
-lxc exec cnt-easyrsa-base -- \
-    bash -c "echo 'cnt-easyrsa-base>' \
+lxc exec easyrsa-base -- \
+    bash -c "echo 'easyrsa-base>' \
         && wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz \
         && tar zxvfP EasyRSA-3.0.3.tgz --transform 's!^EasyRSA-3.0.3!/etc/easyrsa!' \
         && chown -R 0:0 /etc/easyrsa/ \
         && cd /etc/easyrsa/ \
         && ./easyrsa init-pki \
-        && echo '<cnt-easyrsa-base'"
+        && echo '<easyrsa-base'"
 
-lxc restart cnt-easyrsa-base
+lxc restart easyrsa-base
 
-lxc stop cnt-easyrsa-base
-lxc copy cnt-easyrsa-base cnt-easyrsa
-lxc copy cnt-easyrsa-base cnt-openvpn-base
+lxc stop easyrsa-base
+lxc copy easyrsa-base easyrsa
+lxc copy easyrsa-base openvpn-base
 
-lxc start cnt-easyrsa
-lxc start cnt-openvpn-base
+lxc start easyrsa
+lxc start openvpn-base
 sleep 10
 
-lxc exec cnt-openvpn-base -- \
-    bash -c "echo 'cnt-openvpn-base>' \
+lxc exec ovpn-base -- \
+    bash -c "echo 'ovpn-base>' \
         && wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add - \
         && echo 'deb http://build.openvpn.net/debian/openvpn/release/2.4 xenial main' > /etc/apt/sources.list.d/openvpn.list \
         && apt update \
         && apt install -y openvpn \
         && ln -s /etc/easyrsa/ /etc/openvpn/ \
-        && echo '<cnt-openvpn-base'"
+        && echo '<ovpn-base'"
 
-lxc restart cnt-openvpn-base
+lxc restart ovpn-base
 
-lxc stop cnt-openvpn-base
-lxc copy cnt-openvpn-base cnt-server
-lxc copy cnt-openvpn-base cnt-client
+lxc stop ovpn-base
+lxc copy ovpn-base ovpn-svr
+lxc copy ovpn-base ovpn-clt
 
-lxc start cnt-server
-lxc start cnt-client
+lxc start ovpn-svr
+lxc start ovpn-clt
 sleep 10
 
-lxc file push ./server/server.conf cnt-server/etc/openvpn/
-lxc file push ./client/client.conf cnt-client/etc/openvpn/
+lxc file push ./server/server.conf ovpn-svr/etc/openvpn/
+lxc file push ./client/client.conf ovpn-clt/etc/openvpn/
 
-lxc exec cnt-easyrsa -- \
-    bash -c "echo 'cnt-easyrsa>' \
+lxc exec ovpn-svr -- \
+    bash -c "echo 'ovpn-svr>' \
+        && sed -i 's/#AUTOSTART=\"all\"/AUTOSTART=\"all\"/' /etc/default/openvpn \
+        && sed -i 's/LimitNPROC=10/#LimitNPROC=10/' /lib/systemd/system/openvpn@.service \
+        && systemctl daemon-reload \
+        && echo '<ovpn-svr'"
+
+lxc exec ovpn-clt -- \
+    bash -c "echo 'ovpn-clt>' \
+        && sed -i 's/#AUTOSTART=\"all\"/AUTOSTART=\"all\"/' /etc/default/openvpn \
+        && sed -i 's/LimitNPROC=10/#LimitNPROC=10/' /lib/systemd/system/openvpn@.service \
+        && systemctl daemon-reload \
+        && echo '<ovpn-clt'"
+
+lxc exec easyrsa -- \
+    bash -c "echo 'easyrsa>' \
         && cd /etc/easyrsa/ \
         && echo 'easyrsa_ca' | ./easyrsa build-ca nopass \
         && ./easyrsa gen-dh \
-        && echo '<cnt-easyrsa'"
+        && echo '<easyrsa'"
 
-lxc restart cnt-easyrsa
+lxc restart easyrsa
 
-SERVER_NAME=openvpn_svr
-CLIENT_NAME=openvpn_clt1
+SERVER_NAME=ovpn_svr
+CLIENT_NAME=ovpn_clt1
 
-lxc exec cnt-server --env SERVER_NAME=$SERVER_NAME -- \
-    bash -c "echo 'cnt-server>' \
+lxc exec ovpn-svr --env SERVER_NAME=$SERVER_NAME -- \
+    bash -c "echo 'ovpn-svr>' \
         && cd /etc/easyrsa/ \
         && echo '' | ./easyrsa gen-req $SERVER_NAME nopass \
-        && echo '<cnt-server'"
+        && echo '<ovpn-svr'"
 
-lxc exec cnt-client --env CLIENT_NAME=$CLIENT_NAME -- \
-    bash -c "echo 'cnt-client>' \
+lxc exec ovpn-clt --env CLIENT_NAME=$CLIENT_NAME -- \
+    bash -c "echo 'ovpn-clt>' \
         && cd /etc/easyrsa/ \
         && echo '' | ./easyrsa gen-req $CLIENT_NAME nopass \
-        && echo '<cnt-client'"
+        && echo '<ovpn-clt'"
 
-lxc file pull cnt-server/etc/easyrsa/pki/reqs/$SERVER_NAME.req ./temp/
-lxc file pull cnt-client/etc/easyrsa/pki/reqs/$CLIENT_NAME.req ./temp/
+lxc file pull ovpn-svr/etc/easyrsa/pki/reqs/$SERVER_NAME.req ./temp/
+lxc file pull ovpn-clt/etc/easyrsa/pki/reqs/$CLIENT_NAME.req ./temp/
 
-lxc file push ./temp/$SERVER_NAME.req cnt-easyrsa/root/
-lxc file push ./temp/$CLIENT_NAME.req cnt-easyrsa/root/
+lxc file push ./temp/$SERVER_NAME.req easyrsa/root/
+lxc file push ./temp/$CLIENT_NAME.req easyrsa/root/
 
-lxc exec cnt-easyrsa --env SERVER_NAME=$SERVER_NAME -- \
-    bash -c "echo 'cnt-easyrsa>' \
+lxc exec easyrsa --env SERVER_NAME=$SERVER_NAME -- \
+    bash -c "echo 'easyrsa>' \
         && cd /etc/easyrsa/ \
         && ./easyrsa import-req /root/$SERVER_NAME.req $SERVER_NAME \
         && rm /root/$SERVER_NAME.req \
         && echo 'yes' | bash ./easyrsa sign-req server $SERVER_NAME \
-        && echo '<cnt-easyrsa'"
+        && echo '<easyrsa'"
 
-lxc exec cnt-easyrsa --env CLIENT_NAME=$CLIENT_NAME -- \
-    bash -c "echo 'cnt-easyrsa>' \
+lxc exec easyrsa --env CLIENT_NAME=$CLIENT_NAME -- \
+    bash -c "echo 'easyrsa>' \
         && cd /etc/easyrsa/ \
         && ./easyrsa import-req /root/$CLIENT_NAME.req $CLIENT_NAME \
         && rm /root/$CLIENT_NAME.req \
         && echo 'yes' | bash ./easyrsa sign-req client $CLIENT_NAME \
-        && echo '<cnt-easyrsa'"
+        && echo '<easyrsa'"
 
 rm ./temp/*
 
-lxc file pull cnt-easyrsa/etc/easyrsa/pki/ca.crt ./temp/
-lxc file pull cnt-easyrsa/etc/easyrsa/pki/dh.pem ./temp/
-lxc file pull cnt-easyrsa/etc/easyrsa/pki/issued/$SERVER_NAME.crt ./temp/
-lxc file pull cnt-easyrsa/etc/easyrsa/pki/issued/$CLIENT_NAME.crt ./temp/
+lxc file pull easyrsa/etc/easyrsa/pki/ca.crt ./temp/
+lxc file pull easyrsa/etc/easyrsa/pki/dh.pem ./temp/
+lxc file pull easyrsa/etc/easyrsa/pki/issued/$SERVER_NAME.crt ./temp/
+lxc file pull easyrsa/etc/easyrsa/pki/issued/$CLIENT_NAME.crt ./temp/
 
-lxc file push ./temp/ca.crt cnt-server/etc/easyrsa/pki/
-lxc file push ./temp/ca.crt cnt-client/etc/easyrsa/pki/
-lxc file push ./temp/dh.pem cnt-server/etc/easyrsa/pki/
-lxc file push ./temp/dh.pem cnt-client/etc/easyrsa/pki/
-lxc file push ./temp/$SERVER_NAME.crt cnt-server/etc/easyrsa/pki/
-lxc file push ./temp/$CLIENT_NAME.crt cnt-client/etc/easyrsa/pki/
+lxc file push ./temp/ca.crt ovpn-svr/etc/easyrsa/pki/
+lxc file push ./temp/ca.crt ovpn-clt/etc/easyrsa/pki/
+lxc file push ./temp/dh.pem ovpn-svr/etc/easyrsa/pki/
+lxc file push ./temp/dh.pem ovpn-clt/etc/easyrsa/pki/
+lxc file push ./temp/$SERVER_NAME.crt ovpn-svr/etc/easyrsa/pki/
+lxc file push ./temp/$CLIENT_NAME.crt ovpn-clt/etc/easyrsa/pki/
 
 rm ./temp/*
+
+lxc restart ovpn-svr
+lxc restart ovpn-clt
