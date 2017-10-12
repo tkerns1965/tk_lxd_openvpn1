9e86e581c5483d2ca15d87f01318da60e7da34f4
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-11 20:54:20 -0400

    Update config.sh

--- openvpn_base/config.sh
+++ openvpn_base/config.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
+echo 'deb http://build.openvpn.net/debian/openvpn/release/2.4 xenial main' > /etc/apt/sources.list.d/openvpn.list
+apt update
+apt install -y openvpn
+ln -s /etc/easyrsa/ /etc/openvpn/
