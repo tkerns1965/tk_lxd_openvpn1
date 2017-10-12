4bba060666dbf01dd300036151057085f8b9b56b
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-11 21:07:55 -0400

    Delete config.sh

--- openvpn_base/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg | apt-key add -
-echo 'deb http://build.openvpn.net/debian/openvpn/release/2.4 xenial main' > /etc/apt/sources.list.d/openvpn.list
-apt update
-apt install -y openvpn
-ln -s /etc/easyrsa/ /etc/openvpn/
