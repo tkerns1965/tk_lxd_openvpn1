de56361022cefd495aad893af3767074a2c05d80
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-11 20:52:05 -0400

    Update config.sh

--- easyrsa_base/config.sh
+++ easyrsa_base/config.sh
@@ -3,4 +3,5 @@
 wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz
 tar zxvfP EasyRSA-3.0.3.tgz --transform 's!^EasyRSA-3.0.3!/etc/easyrsa!'
 chown -R 0:0 /etc/easyrsa/
-bash -c "cd /etc/easyrsa/ && ./easyrsa init-pki"
+cd /etc/easyrsa/
+./easyrsa init-pki
