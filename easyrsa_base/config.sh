528ec77da0dd303490debfb9b194086bfffe37e2
tkerns1965 <tony.kerns.65@gmail.com>
2017-10-11 21:07:43 -0400

    Delete config.sh

--- easyrsa_base/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.3/EasyRSA-3.0.3.tgz
-tar zxvfP EasyRSA-3.0.3.tgz --transform 's!^EasyRSA-3.0.3!/etc/easyrsa!'
-chown -R 0:0 /etc/easyrsa/
-cd /etc/easyrsa/
-./easyrsa init-pki
